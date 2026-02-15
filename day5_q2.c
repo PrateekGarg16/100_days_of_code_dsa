7ab7f6192b077fe106e6d7503223a1570b35d166
Prateek Garg <prateekgarg1110@gmail.com>
2026-02-15 23:38:23 +0530

    100 days of code

--- day5_q2.c
+++ day5_q2.c
@@ -5,14 +5,13 @@ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
 To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n. */
 
-
 #include <stdio.h>
 
 void merge(int nums1[], int m, int nums2[], int n) {
     int i = m - 1;          // Last valid element in nums1
     int j = n - 1;          // Last element in nums2
     int k = m + n - 1;      // Last position in nums1
-
+    
     while (i >= 0 && j >= 0) {
         if (nums1[i] > nums2[j]) {
             nums1[k] = nums1[i];
@@ -23,7 +22,6 @@ void merge(int nums1[], int m, int nums2[], int n) {
         }
         k--;
     }
-
 
     while (j >= 0) {
         nums1[k] = nums2[j];
@@ -42,6 +40,6 @@ int main() {
     for (int i = 0; i < m + n; i++) {
         printf("%d ", nums1[i]);
     }
-
+    
     return 0;
 }
