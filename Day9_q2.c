4b5bf87a900b961d5f70218169c7aab1da4a5110
Prateek Garg <prateekgarg1110@gmail.com>
2026-02-20 18:57:00 +0530

    100 days of code
    Implement a function to reverse a string in-place.

---
+++ Day9_q2.c
@@ -0,0 +1,19 @@
+/* Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory. */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[] = "hello";
+    int sSize = strlen(s);
+    
+    reverseString(s, sSize);
+    printf("%s\n", s);  // Outputs: olleh
+    
+    return 0;
+}
+
+
+ 
