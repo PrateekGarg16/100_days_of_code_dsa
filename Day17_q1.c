ab1cc4664a3bd59cedba7f0829eed1a87319fc3f
Prateek Garg <prateekgarg1110@gmail.com>
2026-02-27 19:41:12 +0530

    100 days of code
    This program prompts the user for the number of elements and the elements themselves, then calculates and displays the maximum and minimum values in the array.

---
+++ Day17_q1.c
@@ -0,0 +1,37 @@
+/* Problem: Write a program to find the maximum and minimum values present in a given array of integers.
+
+Input:
+- First line: integer n
+- Second line: n integers */
+
+
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
+
+
+
